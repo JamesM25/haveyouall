ab95bf312d68288dc3d02b59edbc25e57bc1fa81
James <motherwell.james@student.greenriver.edu>
2023-05-03 14:47:02 -0400

    Move home page to view/home.html

--- index.php
+++ index.php
@@ -16,7 +16,8 @@ require_once "vendor/autoload.php";
 $f3 = Base::instance();
 
 $f3->route('GET /', function () {
-    echo "<h1>Hello world!</h1>";
+    $view = new Template();
+    echo $view->render('view/home.html');
 });
 
 // Run Fat-Free
