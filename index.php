b3e5a341ca290ceb52091f56120463ce1b143441
James <motherwell.james@student.greenriver.edu>
2023-05-13 01:28:12 -0400

    Create basic views for login and sign up pages

--- index.php
+++ index.php
@@ -12,16 +12,40 @@ error_reporting(E_ALL);
 
 require_once "vendor/autoload.php";
 
-require 'model/db.php';
+require_once 'model/db.php';
 
 // Create an F3 (Fat-Free Framework) object
 $f3 = Base::instance();
 
-$f3->route('GET /', function () {
+// TODO: The current icon is a placeholder. We should find something better eventually.
+const ICON_PATH = 'images/mbox-icon.png';
+
+// Default route
+$f3->route('GET /', function ($f3) {
+    // Set the icon path
+    $f3->set('favicon', ICON_PATH);
+
     $view = new Template();
     echo $view->render('view/home.html');
 });
 
+$f3->route('GET /login', function ($f3) {
+    // Set the icon path
+    $f3->set('favicon', ICON_PATH);
+
+    $view = new Template();
+    echo $view->render('view/login.html');
+});
+
+$f3->route('GET /signup', function ($f3) {
+    // Set the icon path
+    $f3->set('favicon', ICON_PATH);
+
+    $view = new Template();
+    echo $view->render('view/signup.html');
+});
+
+// Simple route for testing database access
 $f3->route('GET /db-test', function() {
     $db = getDatabase();
 
