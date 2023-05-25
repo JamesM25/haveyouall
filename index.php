bf70f312419f9a571f4eae8a2465904b9d859f51
James <motherwell.james@student.greenriver.edu>
2023-05-25 14:54:02 -0400

    Use templating to include stylesheets and scripts

--- index.php
+++ index.php
@@ -12,18 +12,32 @@ error_reporting(E_ALL);
 
 require_once "vendor/autoload.php";
 
-require_once 'model/validation.php';
-
 // Create an F3 (Fat-Free Framework) object
 $f3 = Base::instance();
 
 // TODO: The current icon is a placeholder. We should find something better eventually.
 const ICON_PATH = 'images/mbox-icon.png';
 
+const STYLESHEETS = array(
+    "styles/style.css",
+    "https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css",
+    "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css",
+    "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css"
+);
+
+const SCRIPTS = array(
+    "https://code.jquery.com/jquery-3.3.1.slim.min.js",
+    "https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js",
+    "https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"
+);
+
 // Default route
 $f3->route('GET /', function ($f3) {
     // Set the icon path
     $f3->set('favicon', ICON_PATH);
+
+    $f3->set('stylesheets', STYLESHEETS);
+    $f3->set('scripts', SCRIPTS);
 
     if (Validation::isLoggedIn($f3)) {
         $user = Database::getUser($f3->get("SESSION.user"));
@@ -39,6 +53,9 @@ $f3->route('GET /', function ($f3) {
 $f3->route('GET|POST /login', function ($f3) {
     // Set the icon path
     $f3->set('favicon', ICON_PATH);
+
+    $f3->set('stylesheets', STYLESHEETS);
+    $f3->set('scripts', SCRIPTS);
 
     $email = "";
     $password = "";
@@ -87,6 +104,9 @@ $f3->route('GET|POST /signup', function ($f3) {
 
     // Set the icon path
     $f3->set('favicon', ICON_PATH);
+
+    $f3->set('stylesheets', STYLESHEETS);
+    $f3->set('scripts', SCRIPTS);
 
     $email = "";
     $name = "";
